69192bd47259cd84810a7d34e45d24eb337a96d9
joseph0x4365732E <63211229+joseph0x4365732E@users.noreply.github.com>
2022-07-06 16:37:44 -0700

    Initial Commit

---
+++ Green Light/Green_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Green_LightApp.swift
+//  Green Light
+//
+//  Created by Joseph Cestone on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Green_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
